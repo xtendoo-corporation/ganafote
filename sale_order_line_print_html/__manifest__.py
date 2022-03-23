98b284b802e734fbeaf061d7ba3c983b8413fe55
dani <dani.xtendoo@gmail.com>
2022-03-23 11:49:18 +0100

    [MO]

---
+++ sale_order_line_print_html/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 ForgeFlow (http://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Sale order line print html",
+    "author": "Sale order line print html",
+    "summary": "Sale order line print html",
+    "version": "15.0.1.0.0",
+    "category": "Extra-tools",
+    "website": "https://xtendoo.es",
+    "depends": [
+        "sale",
+        "website_sale"
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+}
