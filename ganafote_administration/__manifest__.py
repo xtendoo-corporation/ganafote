2cc472a998974beedcc3a56bc96749b65b907445
dani <dani.xtendoo@gmail.com>
2022-04-21 09:15:50 +0200

    [ADD] ganafote administration

---
+++ ganafote_administration/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2021 ForgeFlow (http://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Ganafote Administration",
+    "author": "DDL-Xtendoo",
+    "company": "Xtendoo",
+    "summary": "Ganafote Administration",
+    "version": "15.0.1.0.0",
+    "category": "Extra-tools",
+    "website": "https://xtendoo.es",
+    "depends": [
+        "base",
+        "sale_margin"
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+        "views/sale_order_views.xml",
+        "views/crm_views.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+}
