83883207e7dc19a6685e3834ee5edb5c6f4586e4
dani <dani.xtendoo@gmail.com>
2022-04-22 10:00:53 +0200

    [ADD] ganafote_document_format

---
+++ ganafote_document_format/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Document Format Gañafote",
+    "summary": """Formatos de documentos para Gañafote""",
+    "version": "15.0.1.0.0",
+    "description": """Formatos de documentos para Gañafote""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "http://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+        "mail",
+    ],
+    "data": [
+        "data/quotation_send_by_email_template.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
