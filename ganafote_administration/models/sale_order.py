1f402ac04e42c012180fec4c41701a2b8be91e7f
Manuel Calero <manuelcalerosolis@gmail.com>
2022-05-01 13:24:51 +0200

    quit date calculate sale order line

--- ganafote_administration/models/sale_order.py
+++ ganafote_administration/models/sale_order.py
@@ -1,15 +1,15 @@
 # Copyright 2021 Xtendoo (https://xtendoo.es)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models, _
-
-
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
-
-    @api.depends('order_line.customer_lead', 'date_order', 'order_line.state')
-    def _compute_expected_date(self):
-        super(SaleOrder, self)._compute_expected_date()
-        for order in self:
-            order.commitment_date = order.expected_date
-
+# from odoo import api, fields, models, _
+#
+#
+# class SaleOrder(models.Model):
+#     _inherit = "sale.order"
+#
+#     @api.depends('order_line.customer_lead', 'date_order', 'order_line.state')
+#     def _compute_expected_date(self):
+#         super(SaleOrder, self)._compute_expected_date()
+#         for order in self:
+#             order.commitment_date = order.expected_date
+#
