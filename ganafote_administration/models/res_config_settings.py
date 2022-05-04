c67fd69ad7c706e22e4430736131637e7a7f01aa
Manuel Calero <manuelcalerosolis@gmail.com>
2022-05-04 09:26:08 +0200

    traslations

--- ganafote_administration/models/res_config_settings.py
+++ ganafote_administration/models/res_config_settings.py
@@ -7,12 +7,12 @@ from odoo import fields, models, api
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    company_so_factory_template_id = fields.Many2one(
-        related="company_id.sale_order_factory_template_id",
-        string="Factory Default Template",
-        readonly=False,
-        domain="[('model', '=', 'sale.order')]",
-    )
+    # company_so_factory_template_id = fields.Many2one(
+    #     related="company_id.sale_order_factory_template_id",
+    #     string="Factory Default Template",
+    #     readonly=False,
+    #     domain="[('model', '=', 'sale.order')]",
+    # )
     so_factory_mail_template_id = fields.Many2one(
         comodel_name='mail.template',
         string='Factory Email Template',
@@ -20,11 +20,11 @@ class ResConfigSettings(models.TransientModel):
         config_parameter='sale.factory_mail_template',
         help="Email sent to factory when the order is validated."
     )
-    company_so_factory_email = fields.Char(
-        related="company_id.sale_order_factory_email",
-        string="Factory Default Mail",
-        readonly=False,
-    )
+    # company_so_factory_email = fields.Char(
+    #     related="company_id.sale_order_factory_email",
+    #     string="Factory Default Mail",
+    #     readonly=False,
+    # )
     so_factory_mail_default = fields.Char(
         string="Factory Default Mail",
         config_parameter='sale.factory_mail_default',
