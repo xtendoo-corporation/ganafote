a20c0808051fa46b08b3d51f07037958cc480d3b
Camilx03 <camilojrjbeas@gmail.com>
2023-05-09 10:27:52 +0200

    migration to 16.0

--- ganafote_administration/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import sale_order
-from . import res_company
-from . import res_config_settings
-from . import account_move
-from . import utm
--- ganafote_administration/models/utm.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2022 Xtendoo (https://xtendoo.es)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from odoo import models, fields
-
-
-class SaleOrder(models.Model):
-    _inherit = "utm.source"
-    active = fields.Boolean(default=True, string="Activo")
